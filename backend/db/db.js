abc22ea18faf7a366565a4c8007f538d2d1480d3
AakankshaSarode <aakankshasarode1@gmail.com>
2025-02-01 19:18:01 +0530

    intial commit

---
+++ backend/db/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+function connect() {
+  mongoose
+    .connect(process.env.MONGODB_URL)
+    .then(() => {
+      console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+export default connect;
